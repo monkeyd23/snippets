ce375657960cd063f0c3a251aa24bf334e5889b4
Imran Sayyed <imran.sayyed@gslab.com>
2017-07-18 17:36:40 +0530

    Update snippets.sql

--- snippets.sql
+++ snippets.sql
@@ -4,3 +4,6 @@ SELECT
     extract('epoch' from date_trunc('YEAR', (CURRENT_DATE - INTERVAL '1 YEAR'))::TIMESTAMP)::bigint as start_epoch,
     (date_trunc('YEAR', (CURRENT_DATE + INTERVAL '1 YEAR')) - INTERVAL '1 SEC')::TIMESTAMP as end_time,
     extract('epoch' from (date_trunc('YEAR', (CURRENT_DATE + INTERVAL '1 YEAR')) - INTERVAL '1 SEC')::TIMESTAMP)::bigint as end_epoch;
+
+/* Big Int to date */
+TO_CHAR(TO_TIMESTAMP(bigint_field / 1000), 'DD/MM/YYYY HH24:MI:SS')
