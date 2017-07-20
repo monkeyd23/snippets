e5e909539d48af332519d995a0d4706b55e96edc
Imran Sayyed <imran.sayyed@gslab.com>
2017-07-20 15:50:06 +0530

    Update snippets.sql

--- snippets.sql
+++ snippets.sql
@@ -10,3 +10,24 @@ TO_CHAR(TO_TIMESTAMP(bigint_field / 1000), 'DD/MM/YYYY HH24:MI:SS')
 
 /* Number of days between to bigint epochs */
 extract(day from ('1970-01-01 00:00:00 GMT'::timestamp + ((dw.expiry_date/1000)::text)::interval) - ('1970-01-01 00:00:00 GMT'::timestamp + ((qt.time/1000)::text)::interval) )
+
+
+/* Current Year */
+SELECT 
+date_trunc('YEAR',  CURRENT_DATE) as current_year_start_date,
+date_trunc('YEAR', (CURRENT_DATE + INTERVAL '1 YEAR')) - INTERVAL '1 SEC' as current_year_end_date,
+/* Last Year */
+date_trunc('YEAR',  CURRENT_DATE - INTERVAL '1 YEAR') as last_year_start_date,
+date_trunc('YEAR', CURRENT_DATE) - INTERVAL '1 SEC' as last_year_end_date,
+/* Current Month */
+date_trunc('MONTH',  CURRENT_DATE) as current_month_start_date,
+date_trunc('MONTH', (CURRENT_DATE + INTERVAL '1 MONTH')) - INTERVAL '1 SEC' as current_month_end_date,
+/* Last Month */
+date_trunc('MONTH',  CURRENT_DATE) - INTERVAL '1 YEAR' as last_month_start_date,
+date_trunc('MONTH', (CURRENT_DATE + INTERVAL '1 MONTH')) - INTERVAL '1 SEC' - INTERVAL '1 YEAR' as current_month_end_date,
+/* Current Quarter */
+date_trunc('QUARTER',  CURRENT_DATE) as current_quarter_start_date,
+date_trunc('QUARTER', (CURRENT_DATE + INTERVAL '3 MONTH')) - INTERVAL '1 SEC' as current_quarter_end_date,
+/* Last Quarter */
+date_trunc('QUARTER',  CURRENT_DATE - INTERVAL '1 YEAR') as last_quarter_start_date,
+date_trunc('QUARTER', (CURRENT_DATE + INTERVAL '3 MONTH')) - INTERVAL '1 SEC' - INTERVAL '1 YEAR' as last_quarter_end_date;
